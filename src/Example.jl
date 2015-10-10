414e562d14e9cbe3fb88e02e7d27f3008b4701b6
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-10 19:43:50 +0530

    Added Example usage

---
+++ src/Example.jl
@@ -0,0 +1,7 @@
+using SessionAnalytics
+
+codeJugalbandi = SessionAnalytics.SessionInfo("CodeJugalbandi", [1, 2, 3], [4, 5, 6])
+show(STDOUT, codeJugalbandi)
+directFeedbackWeightedAvg = weightedAverage([0.3, 0.7])
+feedbackOnCodeJugalbandi = codeJugalbandi |> directFeedbackWeightedAvg
+println(feedbackOnCodeJugalbandi)
