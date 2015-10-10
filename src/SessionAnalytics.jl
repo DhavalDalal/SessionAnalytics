f3b43887e12a322d4a4dc0f0919f05de691dc0d5
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-10 19:37:23 +0530

    Weighted Average

--- src/SessionAnalytics.jl
+++ src/SessionAnalytics.jl
@@ -1,5 +1,23 @@
 module SessionAnalytics
 
-# package code goes here
+  export SessionInfo, show, weightedAverage
 
-end # module
+  immutable SessionInfo
+    name::String
+    meanFeedbackFromParticipants::Float64
+    meanFeedbackFromSpeakers::Float64
+  end
+  
+  SessionInfo(name::String, feedbackFromParticipants::Array{Int64, 1}, feedbackFromSpeakers::Array{Int64, 1}) = 
+    SessionInfo(name, feedbackFromParticipants |> mean, feedbackFromSpeakers |> mean)
+  
+  show(io::IO, s::SessionInfo) = println(io, "$(s.name) [$(s.meanFeedbackFromParticipants),$(s.meanFeedbackFromSpeakers)]")
+  
+  show(s::SessionInfo) = show(STDOUT, s)
+  
+  weightedAverage(weights::Array{Float64, 1}) = return function (s::SessionInfo)
+      [s.meanFeedbackFromParticipants s.meanFeedbackFromSpeakers] * weights / sum(weights)
+  end    
+  
+end
+
