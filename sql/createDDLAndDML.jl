735882212de59c0def92e4482c9cc24af228b704
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-11 16:36:09 +0530

    made sure the changes works

--- sql/createDDLAndDML.jl
+++ sql/createDDLAndDML.jl
@@ -1,44 +1,42 @@
-module CreateDDLAndDML
-  using SQLite
-  using Base.Test
+using SQLite
+using Base.Test
 
-  export run
-  
-  function run(dbName::String)
-    println("Connecting to database....$(dbName)")
-    db = SQLiteDB(dbName)
-    println("Inserting data into table...sessions")
-    dropTable(db)
-    println("Creating table...sessions")
-    createTable(db)
-    println("Inserting data into table...sessions")
-    insertSeedData(db)
-    println("Verifying Data created...in sessions")
-    verifyDataCreation(db)
-    close(db)
-    println("DONE.")
-  end
-  
-  function createTable(db)
-    sql = "CREATE TABLE sessions (id INT(11), delegateId INT(11), isSpeaker CHAR(1), rating INT(11), createdOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"
-    query(db, sql)
-  end
-   
-  function insertSeedData(db)
-    query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 1, 'Y', 5)")
-    query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 2, 'Y', 4)")
-    query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 3, 'Y', 3)")
-    query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 4, 'N', 3)")
-    query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 5, 'N', 2)")
-    query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 6, 'N', 1)")
-  end
-  
-  function dropTable(db)
-    query(db, "DROP TABLE sessions")
-  end
-  
-  function verifyDataCreation(db)
-    rs = query(db, "SELECT * from sessions")
-    @test length(rs.values[2]) == 6
-  end
+export run
+
+function run(dbName::String)
+  println("Connecting to database....$(dbName)")
+  db = SQLiteDB(dbName)
+  println("Inserting data into table...sessions")
+  dropTable(db)
+  println("Creating table...sessions")
+  createTable(db)
+  println("Inserting data into table...sessions")
+  insertSeedData(db)
+  println("Verifying Data created...in sessions")
+  verifyDataCreation(db)
+  close(db)
+  println("DONE.")
+end
+
+function createTable(db)
+  sql = "CREATE TABLE sessions (id INT(11), delegateId INT(11), isSpeaker CHAR(1), rating INT(11), createdOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"
+  query(db, sql)
+end
+ 
+function insertSeedData(db)
+  query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 1, 'Y', 5)")
+  query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 2, 'Y', 4)")
+  query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 3, 'Y', 3)")
+  query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 4, 'N', 3)")
+  query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 5, 'N', 2)")
+  query(db, "INSERT INTO sessions (id, delegateId, isSpeaker, rating) VALUES (1, 6, 'N', 1)")
+end
+
+function dropTable(db)
+  query(db, "DROP TABLE sessions")
+end
+
+function verifyDataCreation(db)
+  rs = query(db, "SELECT * from sessions")
+  @test length(rs.values[2]) == 6
 end
--- src/SessionAnalytics.jl
+++ src/SessionAnalytics.jl
@@ -1,7 +1,7 @@
 using Escher
 
 include("analytics/Analytics.jl")
-include("sql/createDDLAndDML.jl")
+include("../sql/createDDLAndDML.jl")
 println("Make sure you have sqllite3 on your path (/usr/bin/sqlite3)")
 println("Go to sql folder in this project and run the command below:")
 println("sqlite3 sessions.db")
