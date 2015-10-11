7225df4bf2d2a4a16fb77888af55d3c0637470a6
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-11 17:20:06 +0530

    merged back

--- src/SessionAnalytics.jl
+++ src/SessionAnalytics.jl
@@ -14,11 +14,3 @@ println("At the julia prompt create Database connection using: db = SQLiteDB(\"s
 println("You may then want to run SessionAnalyticsDDLAndDML.run(db) to create database and tables")
 end
 
-module SessionAnalyticsDDLAndDML
-include("sql/createDDLAndDML.jl")
-println("Make sure you have sqllite3 on your path (/usr/bin/sqlite3)")
-println("Go to sql folder in this project and run the command below:")
-println("--> sqlite3 sessions.db <--")
-println("At the julia prompt create Database connection using: db = SQLiteDB(\"sessions.db\")")
-println("You may then want to run SessionAnalyticsDDLAndDML.run(db) to create database and tables")
-end
