136b7739a335ea068ecabb37b19c59b95eabb532
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-11 11:22:59 +0530

    Added folders

--- src/analytics/Example.jl
+++ src/analytics/Example.jl
@@ -1,8 +1,10 @@
 using SessionAnalytics
 import SessionAnalytics.show
 
-codeJugalbandi = SessionAnalytics.SessionInfo("CodeJugalbandi", [1, 2, 3], [4, 5, 6])
+codeJugalbandi = SessionInfo("CodeJugalbandi", [1, 2, 3], [4, 5, 6])
+
 show(STDOUT, codeJugalbandi)
+
 directFeedbackWeightedAvg = weightedAverage([0.3, 0.7])
 feedbackOnCodeJugalbandi = codeJugalbandi |> directFeedbackWeightedAvg
 println(feedbackOnCodeJugalbandi)
