3c9a8fdd20c21060eb541d68ca88f160fef7ed3b
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-11 17:15:10 +0530

    Added DDL and DML module

--- src/SessionAnalytics.jl
+++ src/SessionAnalytics.jl
@@ -3,12 +3,16 @@ module SessionAnalytics
 using Escher
 
 include("analytics/Analytics.jl")
-include("sql/createDDLAndDML.jl")
-println("Make sure you have sqllite3 on your path (/usr/bin/sqlite3)")
-println("Go to sql folder in this project and run the command below:")
-println("sqlite3 sessions.db")
-println("You may then want to run CreateDDLAndDML.run() to create database and tables")
 # include("ui/Render.jl")
 # include("analytics/Example.jl")
 
+end
+
+module SessionAnalyticsDDLAndDML
+include("sql/createDDLAndDML.jl")
+println("Make sure you have sqllite3 on your path (/usr/bin/sqlite3)")
+println("Go to sql folder in this project and run the command below:")
+println("--> sqlite3 sessions.db <--")
+println("At the julia prompt create Database connection using: db = SQLiteDB(\"sessions.db\")")
+println("You may then want to run SessionAnalyticsDDLAndDML.run(db) to create database and tables")
 end
