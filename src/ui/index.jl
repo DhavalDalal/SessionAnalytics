bc1cbc8e6c90a80ad36b0bbe67c78c0cefd75ab6
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-12 01:12:08 +0530

    Started work on ui

--- src/ui/index.jl
+++ src/ui/index.jl
@@ -1,31 +1,31 @@
 using SessionAnalytics
 
+createSchemaAndInitialData("sessions.db")
+
+colnames = String[]
+colvalues = ["1", "2", "3", "4", "5"]
+with("sessions.db") do db
+  rs = query(db, "select * from sessions")
+  push!(colnames, rs.colnames...)
+  #push!(colvalues, rs.values)
+end
+
+codeJugalbandi = SessionInfo("CodeJugalbandi", [1, 2, 3], [4, 5, 6]) 
+directFeedbackWeightedAvg = weightedAverage([0.3, 0.7])
+feedbackOnCodeJugalbandi = codeJugalbandi |> directFeedbackWeightedAvg
+
+a,b,c,d = map(fillcolor, ["#837", "#859", "#837", "#859", "#837"],
+    map(pad([left, right], 1em), ["A", "B", "C", "D", "E"]))
+
 function main(window)
-    # plaintext("Hello, World!")
-    codeJugalbandi = SessionInfo("CodeJugalbandi", [1, 2, 3], [4, 5, 6])
-    directFeedbackWeightedAvg = weightedAverage([0.3, 0.7])
-    feedbackOnCodeJugalbandi = codeJugalbandi |> directFeedbackWeightedAvg
-    plaintext("$feedbackOnCodeJugalbandi")
-      
-    push!(window.assets, "widgets")
+    paddedHeader = map(pad([left, right], 1em), colnames)
+    colors = ["#892", "#892", "#892", "#892", "#892"]
+    header = map(fillcolor, colors, paddedHeader)
 
-       inp = Input(Dict())
-
-       s = sampler()
-       form = vbox(
-           h1("Submit your Session Rating"),
-           watch!(s, textinput("", name=:name, label="Your name")),
-           hbox("Your rating", watch!(s, slider(1:10, name=:rating)))
-               |> packacross(center),
-           trigger!(s, button("Submit", name=:submit))
-       ) |> maxwidth(400px)
-
-       lift(inp) do dict
-           vbox(
-               plugsampler(s, form) >>> inp,
-               vskip(2em),
-               string(dict)
-           ) |> pad(2em)
-       end
-    
+    paddedRows = map(pad([left, right], 1em), colvalues)
+    rows = map(fillcolor, colors, paddedRows)
+    vbox(
+      hbox(intersperse(hskip(1em), header)),
+      hbox(intersperse(hskip(1em), rows))
+   )   
 end
