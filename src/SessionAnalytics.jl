9c29a48b35b276a8aa3a5a536207e93ca5831613
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2015-10-11 17:15:10 +0530

    Added DDL and DML module

--- src/SessionAnalytics.jl
+++ src/SessionAnalytics.jl
@@ -6,6 +6,12 @@ include("analytics/Analytics.jl")
 # include("ui/Render.jl")
 # include("analytics/Example.jl")
 
+include("sql/createDDLAndDML.jl")
+println("Make sure you have sqllite3 on your path (/usr/bin/sqlite3)")
+println("Go to sql folder in this project and run the command below:")
+println("--> sqlite3 sessions.db <--")
+println("At the julia prompt create Database connection using: db = SQLiteDB(\"sessions.db\")")
+println("You may then want to run SessionAnalyticsDDLAndDML.run(db) to create database and tables")
 end
 
 module SessionAnalyticsDDLAndDML
